ff87b42cefca4a246755d43a92d6aa17046556d1
Loreinna <akidzekae@gmai.com>
2024-05-08 22:22:35 +0300

    Примерный код, график выдает что-то странное, но уже хотя бы строится график)

---
+++ TP LR 3/LarasForm.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace TP_LR_3
+{
+    partial class LarasForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.ChartPopulation = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.IblMaxGrowth = new System.Windows.Forms.Label();
+            this.IblMaxDecline = new System.Windows.Forms.Label();
+            this.DataGridView = new System.Windows.Forms.DataGridView();
+            this.ButtonOpenFile = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ChartPopulation)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ChartPopulation
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.ChartPopulation.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.ChartPopulation.Legends.Add(legend1);
+            this.ChartPopulation.Location = new System.Drawing.Point(476, 61);
+            this.ChartPopulation.Name = "ChartPopulation";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.ChartPopulation.Series.Add(series1);
+            this.ChartPopulation.Size = new System.Drawing.Size(829, 496);
+            this.ChartPopulation.TabIndex = 0;
+            this.ChartPopulation.Text = "chart1";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // IblMaxGrowth
+            // 
+            this.IblMaxGrowth.AutoSize = true;
+            this.IblMaxGrowth.Location = new System.Drawing.Point(21, 39);
+            this.IblMaxGrowth.Name = "IblMaxGrowth";
+            this.IblMaxGrowth.Size = new System.Drawing.Size(35, 13);
+            this.IblMaxGrowth.TabIndex = 1;
+            this.IblMaxGrowth.Text = "label1";
+            // 
+            // IblMaxDecline
+            // 
+            this.IblMaxDecline.AutoSize = true;
+            this.IblMaxDecline.Location = new System.Drawing.Point(21, 159);
+            this.IblMaxDecline.Name = "IblMaxDecline";
+            this.IblMaxDecline.Size = new System.Drawing.Size(35, 13);
+            this.IblMaxDecline.TabIndex = 2;
+            this.IblMaxDecline.Text = "label2";
+            // 
+            // DataGridView
+            // 
+            this.DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView.Location = new System.Drawing.Point(24, 261);
+            this.DataGridView.Name = "DataGridView";
+            this.DataGridView.Size = new System.Drawing.Size(394, 253);
+            this.DataGridView.TabIndex = 3;
+            // 
+            // ButtonOpenFile
+            // 
+            this.ButtonOpenFile.Location = new System.Drawing.Point(24, 207);
+            this.ButtonOpenFile.Name = "ButtonOpenFile";
+            this.ButtonOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.ButtonOpenFile.TabIndex = 4;
+            this.ButtonOpenFile.Text = "button1";
+            this.ButtonOpenFile.UseVisualStyleBackColor = true;
+            this.ButtonOpenFile.Click += new System.EventHandler(this.ButtonOpenFile_Click);
+            // 
+            // LarasForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1392, 689);
+            this.Controls.Add(this.ButtonOpenFile);
+            this.Controls.Add(this.DataGridView);
+            this.Controls.Add(this.IblMaxDecline);
+            this.Controls.Add(this.IblMaxGrowth);
+            this.Controls.Add(this.ChartPopulation);
+            this.Name = "LarasForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ChartPopulation)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart ChartPopulation;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label IblMaxGrowth;
+        private System.Windows.Forms.Label IblMaxDecline;
+        private System.Windows.Forms.DataGridView DataGridView;
+        private System.Windows.Forms.Button ButtonOpenFile;
+    }
+}
+
---
+++ TP LR 3/LarasForm.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using CsvHelper;
+using CsvHelper.Configuration;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.WinForms;
+using LiveCharts.Wpf;
+using Microsoft.Win32;
+
+namespace TP_LR_3
+{
+    public partial class LarasForm : Form
+    {
+        private List<PopulationData> populationDataList;
+
+        public LarasForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Добавляем ось X с заголовком "Год"
+            ChartPopulation.ChartAreas[0].AxisX.Title = "Год";
+
+            // Добавляем ось Y с заголовком "Численность населения"
+            ChartPopulation.ChartAreas[0].AxisY.Title = "Численность населения";
+        }
+
+        private void ButtonOpenFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    Delimiter = ";", // Указываем табуляцию как разделитель
+                    HeaderValidated = null // Игнорируем проверку заголовков
+                };
+
+                using (var reader = new StreamReader(openFileDialog1.FileName))
+                using (var csv = new CsvReader(reader, csvConfig))
+                {
+                    populationDataList = csv.GetRecords<PopulationData>().ToList();
+                }
+
+                DisplayDataInTable();
+                DrawPopulationChart();
+                CalculateGrowthAndDecline();
+            }
+        }
+
+        private void DisplayDataInTable()
+        {
+            DataGridView.DataSource = populationDataList;
+        }
+
+        private void DrawPopulationChart()
+        {
+            ChartPopulation.Series.Clear();
+            var series = ChartPopulation.Series.Add("Численность населения России");
+            series.ChartType = SeriesChartType.Line;
+
+            foreach (var data in populationDataList)
+            {
+                series.Points.AddXY(data.Year, data.Population);
+            }
+        }
+
+
+        private void CalculateGrowthAndDecline()
+        {
+            double maxGrowth = 0;
+            double maxDecline = 0;
+            for (int i = 1; i < populationDataList.Count; i++)
+            {
+                double growthRate = (populationDataList[i].Population - populationDataList[i - 1].Population) / populationDataList[i - 1].Population * 100;
+                if (growthRate > maxGrowth)
+                {
+                    maxGrowth = growthRate;
+                }
+                else if (growthRate < maxDecline)
+                {
+                    maxDecline = growthRate;
+                }
+            }
+
+            IblMaxGrowth.Text = $"Максимальный прирост населения: {maxGrowth}%";
+            IblMaxDecline.Text = $"Максимальная убыль населения: {maxDecline}%";
+        }
+    }
+    //Напишу коммент 
+    public class PopulationData
+    {
+        public int Year { get; set; }
+        public double Population { get; set; }
+    }
+}
